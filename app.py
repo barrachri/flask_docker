7f769e23160202e923df257fb5c0806a9bf883a6
Christian Barra <barrachri@gmail.com>
2015-01-24 19:02:47 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import datetime
+from flask import Flask, request, render_template, g
+from peewee import SqliteDatabase, Model, CharField, TextField, DateTimeField, OperationalError
+
+## CONFIG
+DATABASE = 'peewee.db'
+DEBUG = True
+
+## MODEL
+database = SqliteDatabase(DATABASE)
+
+
+class Comment(Model):
+    title = CharField()
+    body = TextField()
+    date = DateTimeField()
+    author = CharField()
+
+    class Meta:
+        database = database
+
+
+## APP
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.before_request
+def before_request():
+	g.db = database
+	g.db.connect()
+
+@app.after_request
+def after_request(response):
+	g.db.close()
+	return response
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+    	title = request.form['title']
+    	author = request.form['author']
+    	body = request.form['text']
+
+    	comment = Comment.create(
+    		title=title,
+    		body=body,
+    		author=author,
+    		date=datetime.datetime.now())
+    	comment.save()
+
+    comments = Comment.select().order_by(Comment.date.desc())
+    return render_template('index.html', comments=comments)
+
+if __name__ == '__main__':
+	try:
+		Comment.create_table()
+	except OperationalError:
+		print("Comment table already exists!")
+	app.run()
