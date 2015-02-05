894e0ff59f94bacd91375df21d627025abb0e443
Christian Barra <barrachri@gmail.com>
2015-02-05 23:59:09 +0100

    app.py and build.yml updates:

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ class Comment(Model):
     class Meta:
         database = psql_db
 
+try:
+	Comment.create_table()
+except (OperationalError, ProgrammingError):
+	print("Comment table already exists!")
+
 ## APP
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -54,20 +59,6 @@ def index():
     comments = Comment.select().order_by(Comment.date.desc())
     return render_template('index.html', comments=comments)
 
-# Check if database already exists
-try:
-	con = psycopg2.connect(host=host, user=user, database="postgres")
-	con.set_isolation_level(0)
-	cur = con.cursor()
-	cur.execute('CREATE DATABASE %s' % database)
-except psycopg2.ProgrammingError as error:
-	print("Database already exists")
-finally:
-	cur.close()
 
-try:
-	Comment.create_table()
-except (OperationalError, ProgrammingError):
-	print("Comment table already exists!")
 if __name__ == '__main__':
 	app.run()
